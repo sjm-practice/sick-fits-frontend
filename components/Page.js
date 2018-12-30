ceb6fbe69646cfd409fe0254f44d001c5cd1c3e8
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2018-12-30 11:20:36 -0500

    converting react class component to functional component

--- components/Page.js
+++ components/Page.js
@@ -1,19 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import Meta from "./Meta";
 import Header from "./Header";
 
-class Page extends Component {
-  render() {
-    const { children } = this.props;
-
-    return (
-      <div>
-        <Meta />
-        <Header />
-        {children}
-      </div>
-    );
-  }
-}
+const Page = ({ children }) => (
+  <div>
+    <Meta />
+    <Header />
+    {children}
+  </div>
+);
 
 export default Page;
