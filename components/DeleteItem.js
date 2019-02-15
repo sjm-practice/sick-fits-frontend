fce9eb14c26d173a2d59233752ca3d249b4630f3
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2019-02-15 15:27:32 -0500

    add update view (cache) on delete item

--- components/DeleteItem.js
+++ components/DeleteItem.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
+import { ALL_ITEMS_QUERY } from "./Items";
 
 const DELETE_ITEM_MUTATION = gql`
   mutation DELETE_ITEM_MUTATION($id: ID!) {
@@ -19,11 +20,21 @@ class DeleteItem extends Component {
     }
   };
 
+  update = (cache, payload) => {
+    // manually update client cache, to match server
+    // 1. read the cache for the items we want
+    const data = cache.readQuery({ query: ALL_ITEMS_QUERY });
+    // 2. filter deleted item out of cache
+    data.items = data.items.filter(item => item.id !== payload.data.deleteItem.id);
+    // 3. put the items back in cache
+    cache.writeQuery({ query: ALL_ITEMS_QUERY, data });
+  };
+
   render() {
     const { children, id } = this.props;
 
     return (
-      <Mutation mutation={DELETE_ITEM_MUTATION} variables={{ id }}>
+      <Mutation mutation={DELETE_ITEM_MUTATION} variables={{ id }} update={this.update}>
         {(deleteItem, { error }) => (
           <button type="button" onClick={() => this.handleDelete(deleteItem)}>
             {children}
