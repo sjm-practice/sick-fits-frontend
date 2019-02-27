436098c4b99b430291918b38f5d332c73efa6097
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2019-02-27 12:43:23 -0500

    add User query component

--- components/Nav.js
+++ components/Nav.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Link from "next/link";
 import NavStyles from "./styles/NavStyles";
+import User from "./User";
 
 const Nav = () => (
   <NavStyles>
+    <User>{({ data: { me } }) => (me ? <p>{me.name}</p> : null)}</User>
     <Link href="/items">
       <a>Shop</a>
     </Link>
