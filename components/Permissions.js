55865fe29985e129d15ec4e4d2807e29c8283c6c
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2019-03-04 14:02:21 -0500

    add table of permissions to Permissions

--- components/Permissions.js
+++ components/Permissions.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import Error from "./ErrorMessage";
+import Table from "./styles/Table";
+import SickButton from "./styles/SickButton";
+
+const possiblePermissions = [
+  "ADMIN",
+  "USER",
+  "ITEMCREATE",
+  "ITEMUPDATE",
+  "ITEMDELETE",
+  "PERMISSIONUPDATE",
+];
 
 const ALL_USERS_QUERY = gql`
   query ALL_USERS_QUERY {
@@ -18,11 +29,52 @@ const Permissions = () => (
   <Query query={ALL_USERS_QUERY}>
     {({ data, loading, error }) => (
       <div>
+        {loading && <p>Loading...</p>}
         <Error error={error} />
-        <p>Hey!!!</p>
+        <h2>Manage Permissions</h2>
+        <Table>
+          <thead>
+            <th>Name</th>
+            <th>Email</th>
+            {possiblePermissions.map(perm => (
+              <th>{perm}</th>
+            ))}
+            <th>👇</th>
+          </thead>
+          <tbody>
+            {data.users.map(user => (
+              <UserRow user={user} />
+            ))}
+          </tbody>
+        </Table>
       </div>
     )}
   </Query>
 );
+
+class UserRow extends Component {
+  render() {
+    const {
+      user: { name, email, id }, // eslint-disable-line react/prop-types
+    } = this.props;
+
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{email}</td>
+        {possiblePermissions.map(permission => (
+          <td>
+            <label htmlFor={`${id}-permission-${permission}`}>
+              <input type="checkbox" />
+            </label>
+          </td>
+        ))}
+        <td>
+          <SickButton>Update</SickButton>
+        </td>
+      </tr>
+    );
+  }
+}
 
 export default Permissions;
