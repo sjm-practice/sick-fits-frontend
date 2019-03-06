f496f80b589086dcdef5130091d98d6938ec83a7
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2019-03-06 11:47:12 -0500

    add cartOpen toggle mutation

--- components/Cart.js
+++ components/Cart.js
@@ -1,27 +1,46 @@
 import React from "react";
-import PropTypes from "prop-types";
+import { Query, Mutation } from "react-apollo";
+import gql from "graphql-tag";
 import CartStyles from "./styles/CartStyles";
 import Supreme from "./styles/Supreme";
 import CloseButton from "./styles/CloseButton";
 import SickButton from "./styles/SickButton";
 
-const Cart = props => (
-  <CartStyles open>
-    <header>
-      <CloseButton title="close">&times;</CloseButton>
-      <Supreme>Your Cart</Supreme>
-      <p>You Have __ Items in your cart.</p>
-    </header>
+export const LOCAL_STATE_QUERY = gql`
+  query LOCAL_STATE_QUERY {
+    cartOpen @client
+  }
+`;
 
-    <footer>
-      <p>$10.10</p>
-      <SickButton>Checkout</SickButton>
-    </footer>
-  </CartStyles>
+export const TOGGLE_CART_MUTATION = gql`
+  mutation TOGGLE_CART_MUTATION {
+    toggleCart @client
+  }
+`;
+
+const Cart = () => (
+  <Mutation mutation={TOGGLE_CART_MUTATION}>
+    {toggleCart => (
+      <Query query={LOCAL_STATE_QUERY}>
+        {({ data: { cartOpen } }) => (
+          <CartStyles open={cartOpen}>
+            <header>
+              <CloseButton onClick={toggleCart} title="close">
+                &times;
+              </CloseButton>
+              <Supreme>Your Cart</Supreme>
+              <p>You Have __ Items in your cart.</p>
+            </header>
+
+            <footer>
+              <p>$10.10</p>
+              <SickButton>Checkout</SickButton>
+            </footer>
+          </CartStyles>
+        )}
+      </Query>
+    )}
+  </Mutation>
 );
-
-Cart.propTypes = {
-  props: PropTypes.object.isRequired, // eslint-disable-line 
-};
 
 export default Cart;
--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Link from "next/link";
+import { Mutation } from "react-apollo";
 import NavStyles from "./styles/NavStyles";
 import User from "./User";
 import Signout from "./Signout";
+import { TOGGLE_CART_MUTATION } from "./Cart";
 
 // next.js treats <> as a React Fragment
 
@@ -25,6 +27,13 @@ const Nav = () => (
               <a>Account</a>
             </Link>
             <Signout />
+            <Mutation mutation={TOGGLE_CART_MUTATION}>
+              {toggleCart => (
+                <button type="button" onClick={toggleCart}>
+                  My Cart
+                </button>
+              )}
+            </Mutation>
           </>
         )}
         {!me && (
