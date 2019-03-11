2bc4e4851c76c74d351aa1f1e8730b7864d0f849
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2019-03-11 16:05:57 -0400

    add AddToCart component and needed gql

--- components/Item.js
+++ components/Item.js
@@ -6,6 +6,7 @@ import ItemStyles from "./styles/ItemStyles";
 import PriceTag from "./styles/PriceTag";
 import formatMoney from "../lib/formatMoney";
 import DeleteItem from "./DeleteItem";
+import AddToCart from "./AddToCart";
 
 class Item extends Component {
   render() {
@@ -35,7 +36,7 @@ class Item extends Component {
           >
             <a>Edit ✏️</a>
           </Link>
-          <button type="button">Add To Cart</button>
+          <AddToCart id={item.id} />
           <DeleteItem id={item.id}>Delete Item</DeleteItem>
         </div>
       </ItemStyles>
