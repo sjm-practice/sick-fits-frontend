489757d102de4b02bff1959cc03f5da4ea11c858
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2019-03-12 09:25:37 -0400

    add update cache (optimistically) when item deleted from cart

--- components/RemoveFromCart.js
+++ components/RemoveFromCart.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { Mutation } from "react-apollo";
 import styled from "styled-components";
 import gql from "graphql-tag";
-import User from "./User";
+import User, { CURRENT_USER_QUERY } from "./User";
 
 const REMOVE_FROM_CART_MUTATION = gql`
   mutation REMOVE_FROM_CART_MUTATION($id: ID!) {
@@ -24,10 +24,42 @@ const BigButton = styled.button`
 `;
 
 class RemoveFromCart extends Component {
+  // This gets called as soon as we get a response back from
+  // the server after a mutation has been performed.
+  update = (cache, payload) => {
+    // NOTE: this will get called twice. once immediately, using the value
+    // in optimisticResponse for payload. then called a second time, when the
+    // actual server response is received (which uses the actual payload value)
+
+    // console.log("Running remove cart update...");
+    // 1. read the cache
+    const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+    // console.log(data);
+
+    // 2. remove the item from the cache (use the cart item id returned
+    //    in the server response)
+    const removedCartItemId = payload.data.removeFromCart.id;
+    data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== removedCartItemId);
+
+    // 3. write it back to the cache
+    cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+  };
+
   render() {
     const { id } = this.props;
     return (
-      <Mutation mutation={REMOVE_FROM_CART_MUTATION} variables={{ id }}>
+      <Mutation
+        mutation={REMOVE_FROM_CART_MUTATION}
+        variables={{ id }}
+        update={this.update}
+        optimisticResponse={{
+          __typename: "Mutation",
+          removeFromCart: {
+            __typename: "CartItem",
+            id,
+          },
+        }}
+      >
         {(removeFromCart, { loading, error }) => (
           <BigButton
             title="Delete Item"
