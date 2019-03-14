603d49f87b32ddc25e32d48391e82af383a869a3
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2019-03-14 19:34:57 -0400

    add additional info to OrderList

--- components/OrderList.js
+++ components/OrderList.js
@@ -2,52 +2,74 @@ import React, { Component } from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import Link from "next/link";
+import styled from "styled-components";
 import { format } from "date-fns";
+import { formatDistance } from "date-fns/esm";
 import formatMoney from "../lib/formatMoney";
+import OrderItemStyles from "./styles/OrderItemStyles";
 import Error from "./ErrorMessage";
 
 const USER_ORDERS_QUERY = gql`
   query USER_ORDERS_QUERY {
-    orders {
+    orders(orderBy: createdAt_DESC) {
       id
       total
       createdAt
+      items {
+        id
+        title
+        price
+        description
+        quantity
+        image
+      }
     }
   }
+`;
+
+const OrderUL = styled.ul`
+  display: grid;
+  grid-gap: 4rem;
+  grid-template-columns: repeat(auto-fit, minmax(40%, 1fr));
 `;
 
 class OrderList extends Component {
   render() {
     return (
       <Query query={USER_ORDERS_QUERY}>
-        {({ data, error, loading }) => {
-          if (error) return <Error error={error} />;
+        {({ data: { orders }, error, loading }) => {
           if (loading) return <p>Loading...</p>;
-
-          const { orders } = data;
+          if (error) return <Error error={error} />;
 
           return (
             <div>
-              <ul>
+              <h2>You have {orders.length} orders.</h2>
+              <OrderUL>
                 {orders.map(order => (
-                  <li key={order.id}>
-                    <span>
-                      <Link
-                        href={{
-                          pathname: "/order",
-                          query: { id: order.id },
-                        }}
-                      >
-                        <a>{order.id}</a>
-                      </Link>
-                    </span>
-                    {" - "}
-                    <span>{formatMoney(order.total)}</span>
-                    {" - "}
-                    <span>{format(order.createdAt, "MMMM d, YYYY h:mm a")}</span>
-                  </li>
+                  <OrderItemStyles key={order.id}>
+                    <Link
+                      href={{
+                        pathname: "/order",
+                        query: { id: order.id },
+                      }}
+                    >
+                      <a>
+                        <div className="order-meta">
+                          <p>{order.items.reduce((a, b) => a + b.quantity, 0)} Items</p>
+                          <p>{order.items.length} Products</p>
+                          <p>{formatDistance(order.createdAt, new Date())} ago</p>
+                          <p>{formatMoney(order.total)}</p>
+                        </div>
+                        <div className="images">
+                          {order.items.map(item => (
+                            <img src={item.image} alt={item.title} />
+                          ))}
+                        </div>
+                      </a>
+                    </Link>
+                  </OrderItemStyles>
                 ))}
-              </ul>
+              </OrderUL>
             </div>
           );
         }}
